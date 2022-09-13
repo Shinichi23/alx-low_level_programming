b6514d5e684264557d3eedb3c87d7d7aea548833
Shinichi23 <taha-chaoui@hotmail.com>
2022-09-13 15:26:33 -0700

    Correction

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @a : character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int a)
+{
+
+return (a >= 97 && a <= 122);
+
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for lower case letter
+ * @a : character to check
+ * Return:0 or 1
+ */
+
+int _isalpha(int a)
+{
+return ((a >= 97 && a <= 122) || (a >= 65 && a <= 90));
+
+}
