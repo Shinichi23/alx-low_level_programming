046a593cc8dba8f903e374416fc105dbe04dc864
Shinichi23 <taha-chaoui@hotmail.com>
2022-09-21 12:46:43 -0700

    Correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,22 @@
-#include "main.h"
-
-/**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will be appended
- *@src: String to be concatenated upon
- *
- * Return: returns poiner to @dest
- */
-
-char *_strcat(char *dest, char *src)
-{
-
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-dest_len++;
-
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
-return (dest);
+#include "main.h"                                                    
+                                                                                                                                        
+/**                                                                                                                                     
+ * _strcat - fills memory with a constant byte                                                                                          
+ * @s: memory space to be filled                                                                                                        
+ * @b: byte to fill with                                                                                                                
+ * @n: number of spaces to fill                                                                                                         
+ *                                                                                                                                      
+ * Return: pointer to memory area s                                                                                                     
+ */                                                                                                                                     
+char *_strcat(char *s, char b, unsigned int n)                                                                                          
+{                                                                                                                                       
+unsigned int i;                                                                                                                         
+                                                                                                                                        
+for (i = 0; i < n; i++)                                                                                                                 
+{                                                                                                                                       
+*(s + i) = b;                                                                                                                           
+}                                                                                                                                       
+return (s);                                                                                                                             
 }
+
 
