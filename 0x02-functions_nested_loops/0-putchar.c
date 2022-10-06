a3fdf4c4d7a12979170f1db2961ee116d439b53d
Shinichi23 <taha-chaoui@hotmail.com>
2022-10-06 05:23:30 -0700

    Modify

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
  * Return:0
  */
 
-int main(void)
-{
-char c[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
+int main(void){
+  printf("_putchar\n");
+  return 0;
 }
-_putchar('\n');
-return (0);
-}
+
